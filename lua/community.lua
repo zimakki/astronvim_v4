0986a8502b23d594cb0f3bec727022820f6af153
Zi Makki <zimakki@gmail.com>
2024-04-16 11:36:52 +0200

    added community rainbow highlights

--- lua/community.lua
+++ lua/community.lua
@@ -13,6 +13,7 @@ return {
   { import = "astrocommunity.debugging.nvim-bqf" },
   { import = "astrocommunity.editing-support.todo-comments-nvim" },
   { import = "astrocommunity.editing-support.nvim-treesitter-endwise" },
+  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
   { import = "astrocommunity.git.diffview-nvim" },
   { import = "astrocommunity.git.neogit" },
   { import = "astrocommunity.git.octo-nvim" },

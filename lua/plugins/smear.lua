742ba396f907994060220448e1377ea4d05a427b
Zi Makki <zimakki@gmail.com>
2025-02-07 12:45:58 +0100

    add smear

---
+++ lua/plugins/smear.lua
@@ -0,0 +1,11 @@
+return {
+  "sphamba/smear-cursor.nvim",
+  opts = {
+    cursor_color = "#00ffff",
+    stiffness = 0.8,
+    trailing_stiffness = 0.1,
+    trailing_exponent = 0.5,
+    hide_target_hack = true,
+    gamma = 1,
+  },
+}

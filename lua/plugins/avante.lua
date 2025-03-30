adf6ed776bc72fbbd70ad1ef1ffd9573bdfa8eb5
Zi Makki <zimakki@gmail.com>
2025-03-30 17:20:02 +0200

    updating the avante config

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,20 +1,19 @@
 return {
   "yetone/avante.nvim",
   event = "VeryLazy",
-  lazy = false,
-  version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+  version = false, -- Never set this value to "*"! Never!
   opts = {
     -- add any opts here
     -- for example
-    -- provider = "openai",
-    -- openai = {
-    --   endpoint = "https://api.openai.com/v1",
-    --   model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
-    --   timeout = 30000, -- timeout in milliseconds
-    --   temperature = 0, -- adjust if needed
-    --   max_tokens = 4096,
-    --   -- reasoning_effort = "high" -- only supported for reasoning models (o1, etc.)
-    -- },
+    provider = "openai",
+    openai = {
+      endpoint = "https://api.openai.com/v1",
+      model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
+      timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+      temperature = 0,
+      max_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+      --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+    },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
